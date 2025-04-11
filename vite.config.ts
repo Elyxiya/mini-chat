173b241c7170613d9404f53f92fe1197944209d3
Elyxiya <3013693518@qq.com>
2025-04-11 20:59:21 +0800

    feat: 配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({command, mode})=>{
+  let env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue()],
+    resolve:{
+      alias:{
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+   
+  }
 })
